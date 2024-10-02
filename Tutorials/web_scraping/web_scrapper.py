cdd1a205260fddd59cb94ab0d69a97efc0b4e53e
Gabriel dos Reis <dosreisg@dupage.edu>
2024-10-02 12:15:38 -0500

    Added get all sections functionality.

--- Tutorials/web_scraping/web_scrapper.py
+++ Tutorials/web_scraping/web_scrapper.py
@@ -9,7 +9,14 @@ soup = BeautifulSoup(html_text, 'html.parser')
 
 # Try to find the 'zen_of_python' section by its id
 zen_code = soup.find(id="zen-of-python")
-
 # Display the text
 print(zen_code.get_text())
 
+# Get all sections
+counter = 1
+all_sections = soup.find_all(class_="section")
+for section in all_sections:
+    print()
+    print(f"Section {counter}")
+    print(section.text.strip())
+    counter += 1
